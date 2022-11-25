b83125934f68bbf4e7f4d4c542e1d84c1d0ca66f
Abasigee <emmanuelnsikak2022@gmail.com>
2022-11-25 17:15:42 +0100

    Starting nodejs

---
+++ app.js
@@ -0,0 +1,12 @@
+var firstInfo = "You're welcome to the begining of Nodejs..."
+
+function HelloWorld(Info){
+    console.log(Info);
+}
+
+setInterval(()=>{
+    HelloWorld(firstInfo)
+},3000)
+
+// Performing HelloWorld with Nodejs...
+// The beginning of Nodejs Backend journey...
